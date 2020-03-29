bec73537ad37a18628c2fa820b549269621d2f88
TomekPyka <61656933+TomekPyka@users.noreply.github.com>
2020-03-29 22:05:21 +0200

    22.05

---
+++ cw2/WebApplication2/WebApplication2/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class Student
+    {
+            public int IdStudent(get; set;);
+   
+            public int FirstName(get; set;);
+        
+            public int LastName(get; set;);
+        
+            public int IndexNumber(get; set;);
+    }
+}
