255034a7cbec555ee81a6a17f15bb3360c1d876e
TomekPyka <61656933+TomekPyka@users.noreply.github.com>
2020-03-29 19:29:41 +0200

    19:29

---
+++ WebApplication2/WebApplication2/Controllers/StudentsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetStudents()
+        {
+            return "Anna, Marcin, Andrzej";
+        }
+    }
+}
