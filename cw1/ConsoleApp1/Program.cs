97144200f6c0603eb36119cc951c023d192ae8ac
TomekPyka <61656933+TomekPyka@users.noreply.github.com>
2020-03-29 18:16:01 +0200

    Full crawler

--- cw1/ConsoleApp1/Program.cs
+++ cw1/ConsoleApp1/Program.cs
@@ -1,17 +1,26 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync("https://www.pja.edu.pl");
 
-            string str = "Ala";
-            Console.WriteLine("Hello World!");
-            Console.WriteLine(str);
-            Console.WriteLine("Hello World!");
-            Console.WriteLine(str);
+            if (response.IsSuccessStatusCode){
+                var html = await response.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z0-9]+@[a-z.]+");
+                MatchCollection matches = regex.Matches(html);
+                foreach (var i in matches) {
+                    Console.WriteLine(i);
+                }
+
+            }
         }
     }
 }
